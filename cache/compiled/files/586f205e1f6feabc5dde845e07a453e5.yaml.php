f57e54e4b8a15c8cbeae8777d4f7d55f37359709
Justin Mulwee <justin@5050detroit.com>
2016-10-15 22:40:47 -0400

    trash cache and logs

--- cache/compiled/files/586f205e1f6feabc5dde845e07a453e5.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/justin/sites/justin.mulwee.com/user/config/streams.yaml',
-    'modified' => 1476576853,
-    'data' => [
-        
-    ]
-];
